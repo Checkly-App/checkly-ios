7df32b5b30df3d19ff73bb76a7e663c6c292a6b5
NouraMajed <nouramajedalsalem@gmail.com>
2022-03-06 19:12:35 +0300

    date search

--- Checkly/attendanceHistoryView.swift
+++ Checkly/attendanceHistoryView.swift
@@ -24,9 +24,9 @@ struct attendanceHistoryView: View {
         DatePicker("To Date", selection: $toDate, in: ...Date() , displayedComponents: .date).padding()
         // Search Button
             Text("Search").onTapGesture {
-                vm.fetchFilteredAttendances()
+                    vm.returnDatesInRange(fromDate: fromDate, toDate: toDate)
             }
-            ForEach (vm.attendances) { attendance in
+            ForEach (vm.filteredAttendancesDates) { attendance in
             HStack {
                 VStack{
                     Text("Date")
@@ -41,8 +41,7 @@ struct attendanceHistoryView: View {
                     Text("6:08")
                 }.padding()
             }.padding()
-
-        }
+            }
         }
     }
 }
@@ -51,6 +50,7 @@ class attendanceHistoryViewModel: ObservableObject {
     
     @Published var attendances = [attendance]()
     @Published var filteredAttendances = [attendance]()
+    @Published var filteredAttendancesDates = [attendance]()
     
     init () {
         fetchAttendances()
@@ -84,17 +84,54 @@ class attendanceHistoryViewModel: ObservableObject {
     func fetchFilteredAttendances () {
         
     }
+    
+    
+    func returnDatesInRange (fromDate: Date, toDate: Date) {
+        let range = fromDate...toDate
+        let ref = Database.database().reference()
+        let searchQueue = DispatchQueue.init(label: "searchQueue")
+        
+        searchQueue.sync {
+            ref.child("Attendance").observe(.childAdded) { snapshot in
+                
+                let obj = snapshot.value as! [String: Any]
+                let emp_id = obj["employee_id"] as! String
+                let date = obj["date"] as! String
+                let time = obj["time"] as! String
+                let type = obj["type"] as! String
+
+                
+                let attendance = attendance(id: emp_id, date: date, time: time, type: type)
+                var formattedDate = convertDateToObject(Date: attendance.date)
+                if range.contains(formattedDate) {
+                    self.filteredAttendancesDates.append(attendance)
+                } else {
+                    print("The date is outside the range")
+                }
+            }
+        }
+    }
 }
 
+    func convertDateToObject (Date: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let date = dateFormatter.date(from: Date)!
+        return date
+    }
 
-func convertDate (Date: Date) {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "YY/MM/dd"
-    dateFormatter.string(from: Date)
-}
+
+//func convertDateToString (Date: Date) {
+//    let dateFormatter = DateFormatter()
+//    dateFormatter.dateFormat = "dd/MM/yyyy"
+//    dateFormatter.string(from: Date)
+//}
 
 struct attendanceHistoryView_Previews: PreviewProvider {
     static var previews: some View {
         attendanceHistoryView()
     }
 }
+
+
+
