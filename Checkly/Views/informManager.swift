4275a8db5e298b95f49efddee7ac7429eb625a62
NouraMajed <nouramajedalsalem@gmail.com>
2022-04-04 14:45:40 +0300

    added buttons

--- Checkly/Views/informManager.swift
+++ Checkly/Views/informManager.swift
@@ -13,14 +13,13 @@ import FirebaseAuth
 
 struct informManager: View {
     
-    @State private var showingAlert = false
+    @State private var showingLateAlert = false
+    @State private var showingEarlyAlert = false
     @ObservedObject var vm = informManagerViewModel()
     
     var body: some View {
     
         VStack {
-            
-            Spacer()
             
             
             HStack{
@@ -35,11 +34,31 @@ struct informManager: View {
                 .shadow(radius: 15)
                 .contentShape(Rectangle())
                 .onTapGesture {
-                    showingAlert = true
-                }.alert("Are you sure you want to inform your manager of being late?", isPresented: $showingAlert) {
+                    showingLateAlert = true
+                }.alert("Are you sure you want to inform your manager of being late?", isPresented: $showingLateAlert) {
                     Button("Cancel", role: .cancel) { }
                     Button("Yes") {
                         vm.updateStatus(newStatus: "Late")
+                    }
+                }
+            
+            HStack{
+                Spacer()
+                Text("Notify manager I'll be early").font(.system(size: 16, weight: .bold))
+                Spacer()
+                }.foregroundColor(.white)
+                .padding(.vertical)
+                .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.345, green: 0.737, blue: 0.925), Color(red: 0.263, green: 0.624, blue: 0.953)]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(32)
+                .padding(.horizontal)
+                .shadow(radius: 15)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    showingEarlyAlert = true
+                }.alert("Are you sure you want to inform your manager of being early?", isPresented: $showingEarlyAlert) {
+                    Button("Cancel", role: .cancel) { }
+                    Button("Yes") {
+                        vm.updateStatus(newStatus: "Early")
                     }
                 }
                 
