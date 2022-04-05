a18a285e0ed6c00a07b095e294e6bb0ece5a59a8
NouraMajed <nouramajedalsalem@gmail.com>
2022-04-06 01:16:11 +0300

    read from database

--- Checkly/Views/viewEmployeesAttendanceStatus.swift
+++ Checkly/Views/viewEmployeesAttendanceStatus.swift
@@ -14,15 +14,46 @@ struct viewEmployeesAttendanceStatus: View {
     
     @ObservedObject var vm = viewEmployeesAttendanceStatusViewModel()
     
+    
     var body: some View {
-        Text(vm.department)
+        
+        ScrollView{
+        ForEach(vm.employees , id: \.self) { emp in
+            VStack{
+            HStack{
+                Text(emp.name)
+                Text(emp.status)
+            
+        }.padding().frame(width: 350, height: 50).background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(color: .gray, radius: 0.5, x: 0.5, y: 0.5))
+            }.padding()
+        }
+        }.padding()
+    
+
+//                HStack {
+//                    Spacer()
+//                    if ( leave.status == "rejected" ) {
+//                    RoundedRectangle(cornerRadius: 20).frame(width: 90, height: 30).foregroundColor(Color(red: 0.902, green: 0.306, blue: 0.306)).opacity(0.2).overlay(
+//                        Text(leave.status)).foregroundColor(Color(red: 0.902, green: 0.306, blue: 0.30))
+//                    } else if ( leave.status == "accepted" )  {
+//                    RoundedRectangle(cornerRadius: 20).frame(width: 90, height: 30).foregroundColor(Color(red: 0.306, green: 0.902, blue: 0.604)).opacity(0.2).overlay(
+//                        Text(leave.status)).foregroundColor(Color(red: 0.306, green: 0.902, blue: 0.604))
+//                    } else {
+//                    RoundedRectangle(cornerRadius: 20).frame(width: 90, height: 30).foregroundColor(Color(red: 0.969, green: 0.675, blue: 0.408)).opacity(0.2).overlay(
+//                        Text(leave.status)).foregroundColor(Color(red: 0.969, green: 0.675, blue: 0.408))
+//                    }
+//                }
+            
+        
     }
 }
 
 class viewEmployeesAttendanceStatusViewModel: ObservableObject {
     
     let user = Auth.auth().currentUser
+    let ref = Database.database().reference()
     var department = ""
+    @Published var employees = [list]()
     
     init () {
         fetchDepartment()
@@ -30,7 +61,7 @@ class viewEmployeesAttendanceStatusViewModel: ObservableObject {
     
     func fetchDepartment() {
         
-        let ref = Database.database().reference()
+        
         let searchQueue = DispatchQueue.init(label: "searchQueue")
         
         searchQueue.sync {
@@ -41,13 +72,47 @@ class viewEmployeesAttendanceStatusViewModel: ObservableObject {
                 let manager_id = obj["manager"] as! String
                 
                 if ( manager_id == self.user!.uid ) {
-                    self.department = snapshot.key
+                    self.getCompanyEmployees(dep: snapshot.key)
                 }
             }
         }
         print(self.department)
+        
     }
     
+    func getCompanyEmployees(dep: String){
+
+        let Queue = DispatchQueue.init(label: "Queue")
+        
+
+        Queue.sync {
+
+        ref.child("Employee").observe(.value, with: { dataSnapshot in
+        for emp in dataSnapshot.children {
+        let obj = emp as! DataSnapshot
+
+        let name = obj.childSnapshot(forPath: "name").value as! String
+        let department = obj.childSnapshot(forPath: "department").value as! String
+        let status  = obj.childSnapshot(forPath: "status").value as! String
+            
+            if ( department == dep) {
+            let list = list(name: name, status: status)
+        
+            self.employees.append(list)
+            }
+
+                }
+            }, withCancel: { error in
+            print(error.localizedDescription)
+            })
+        }
+    }
+}
+
+
+struct list: Hashable {
+    var name: String
+    var status: String
 }
 
 struct viewEmployeesAttendanceStatus_Previews: PreviewProvider {
